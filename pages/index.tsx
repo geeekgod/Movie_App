12acea8d6d4601b41b2642aff70bd0b784e0e532
Rishabh Singh <ris8645208@gmail.com>
2021-08-21 15:59:25 +0530

    Data received from endpoint

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,51 +3,72 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import useSWR from "swr";
-import axios, { AxiosRequestConfig } from 'axios';
-import { useEffect } from "react";
+import axios, { AxiosRequestConfig } from "axios";
+import { useEffect, useState } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
 const Home: NextPage = () => {
-
   let data: any;
 
-  const term = 'thor';
-  const url = 'https://imdb8.p.rapidapi.com/auto-complete';
+  const [term, setTerm] = useState(" ");
 
-  const searchMovie = async () =>{
-     data = useSWR([url, term], () => axios({
-      url: 'https://imdb8.p.rapidapi.com/auto-complete',
-      // withCredentials: true,
-      method: 'GET',
-      headers: {
-        'x-rapidapi-host': 'imdb8.p.rapidapi.com',
-        'x-rapidapi-key': 'c00e7820b4msh8ed6fb408512112p1269e6jsn01948f19398a'
-      },
-      params: { q: term }
-    }).then(res => res.data))
-  }
+  const url = "https://imdb8.p.rapidapi.com/auto-complete";
 
-  // const data = useSWR(options.url, fetcher);
+  const searchMovie = async () => {
+    data = useSWR([url, term], () =>
+      axios({
+        url: "https://imdb8.p.rapidapi.com/auto-complete",
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "imdb8.p.rapidapi.com",
+          "x-rapidapi-key":
+            "c00e7820b4msh8ed6fb408512112p1269e6jsn01948f19398a",
+        },
+        params: { q: term },
+      }).then((res) => res.data)
+    );
+  };
 
-  // let finData = data.data.d;
+  let finData;
 
   // useEffect(()=>{
-    searchMovie();
-  // },[]);
+  searchMovie();
+  // });
 
-  // if(data.data){
-  //   console.log(data.data);
-  // }
+  if (data.data != null) {
+    console.log(data.data);
+    finData = data.data.d;
+  }
 
   return (
     <div>
       <nav>
         <h1>Movie App</h1>
-        {/* {
-          finData && finData.map((item: any) =>{
-            return(
-              <img src={item.i.imageUrl} alt=""/>
-            )
-          })
-        } */}
+        <input type="text" onChange={(e)=> setTerm(e.target.value)} onKeyPress={}/>
+        <Row>
+          {data.data != null &&
+            finData.map((item: any, pos: any) => {
+              return (
+                <Col key={pos} md={4}>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Img variant="top" src={item.i.imageUrl} />
+                    <Card.Body>
+                      <Card.Title>{item.l}</Card.Title>
+                      <Row>
+                        <Col><Card.Text>{item.rank}</Card.Text></Col>
+
+                        <Col><Card.Text>{item.y}</Card.Text></Col>
+                      </Row>
+                      <Button variant="primary">Go somewhere</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+        </Row>
       </nav>
     </div>
   );
